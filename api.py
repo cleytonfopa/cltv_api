49514f0eb6a34a800231043a9036d0ae14f16393
cleyton.farias@gdstecnologia.com.br <cleyton.farias@gdstecnologia.com.br>
2023-10-05 18:42:24 -0300

    version using the Hurdle Model: One classifier to predict null values and another to predict not-null values. This system showed better results

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 import sys
 from flask import Flask, request
 from joblib import load
-from utils import f, clean_DF, calculate_recency
+from utils import f, clean_DF, calculate_recency, predict_interval
 import pandas as pd
 import numpy as np
+import datetime
 
 app = Flask(__name__)
 
@@ -14,26 +15,46 @@ def predict():
     # convert to DF:
     df = pd.DataFrame(json_)
     # cleaning:
-    df = clean_DF(df)
+    df = clean_DF(df)    
     # Calculanto agregados por dia:
-    bet_by_day = df.groupby(["Username", "registration_dt", "ftd_value","age", "date"]).agg(n_bets=("turnover", lambda x: np.sum(x>0)), turnover=("turnover", np.sum),  ggr=("ggr", np.sum)).reset_index()
-    # merge players and bets:
-    bet_by_day.sort_values(by=["Username", "date"], inplace=True)
-    # criando index de referencia:
-    # t1: numero de dias entre a data da aposta e a data de referencia
-    bet_by_day["t1"] = (pd.to_datetime(bet_by_day["date"]) - pd.to_datetime(bet_by_day["registration_dt"])).dt.days
-    bet_by_day = bet_by_day.drop_duplicates()
+    bet_by_day = (
+      df
+      .groupby(["Username", "registration_dt", "ftd_value","age", "date"])
+      .agg(
+        n_bets=("turnover", lambda x: np.sum(x>0)), 
+        turnover=("turnover", np.sum),  
+        ggr=("ggr", np.sum)
+        )
+      .reset_index()
+    )
+    bet_by_day
     # selecionando variáveis:
-    bet_by_day = bet_by_day[["t1", "Username", "ftd_value", "age", "n_bets", "turnover", "ggr"]]
-    bet_by_day = bet_by_day.sort_values(by=["t1", "Username"])    
-    # guarantee 7 days per user:
-    bet_by_day = bet_by_day.groupby("Username").apply(f,min_days=7).reset_index(drop=True)    
+    bet_by_day = bet_by_day[["registration_dt", "date", "Username", "ftd_value", "age", "n_bets", "turnover", "ggr"]]
+    bet_by_day = bet_by_day.sort_values(by=["registration_dt", "Username"])
+    # numero máximo de dias de atividade na plataforma:
+    max_days_sample=(bet_by_day["date"].max() - bet_by_day["registration_dt"].min()).days + 1
+    # Criando as datas correntes para todos os usuários
+    bet_by_day = (
+      bet_by_day
+      .groupby("Username")
+      .apply(f,min_days=max_days_sample)
+      .reset_index(drop=True)
+    )
+    # Filtrando para a data máxima de apostas da base:
+    dt_max_bet = df["date"].max()
+    bet_by_day = bet_by_day.query("date <= @dt_max_bet")
     ## Calculate Recency:
-    recency_df = bet_by_day.groupby("Username").apply(calculate_recency,window=7)
+    # recency:
+    recency_df = (
+      bet_by_day
+      .groupby("Username")
+      .apply(
+        calculate_recency, 
+        date_max=datetime.datetime.today()
+      )
+    )
     recency_df = recency_df.reset_index()
     recency_df.columns = ["Username", "recency_value"]
-    # NA são os casos em que nao houve nenhuma aposta dentro do window.
-    recency_df["recency_value"] = recency_df["recency_value"].fillna(7)
     ## Calculate Frequency:
     frequency_df = bet_by_day.groupby("Username")["n_bets"].sum()
     frequency_df = frequency_df.reset_index()
@@ -42,21 +63,70 @@ def predict():
     revenue_df = bet_by_day.groupby("Username")["ggr"].sum()
     revenue_df = revenue_df.reset_index()
     revenue_df.columns = ["Username", "revenue_value"]
+    # turnover value:
+    turnover_df = bet_by_day.groupby("Username")["turnover"].sum()
+    turnover_df = turnover_df.reset_index()
+    turnover_df.columns = ["Username", "turnover_value"]
     # merging:
     rfm_df = recency_df.merge(frequency_df, on="Username")
-    rfm_df = rfm_df.merge(revenue_df, on="Username")    
+    rfm_df = rfm_df.merge(revenue_df, on="Username")
+    rfm_df = rfm_df.merge(turnover_df, on="Username")
+    # calculando ticket medio
+    rfm_df["ticket_medio"] = rfm_df["turnover_value"] / rfm_df["frequency_value"]
+    # fill na with 0:
+    rfm_df["ticket_medio"] = rfm_df["ticket_medio"].fillna(0)
     # adding info about the player:
     rfm_df = rfm_df.merge(
         bet_by_day[["Username", "age", "ftd_value"]].drop_duplicates(),
         on="Username"
     )
+    
     ## predicting:
-    vars_ = ["recency_value", "frequency_value", "revenue_value",
-             "age", "ftd_value"]
-    rfm_df['y_lower'] = np.round(models["q 0.10"].predict(rfm_df[vars_]),2)
-    rfm_df['y_upper'] = np.round(models["q 0.90"].predict(rfm_df[vars_]), 2)
-    rfm_df['y_pred'] = np.round(models["q 0.50"].predict(rfm_df[vars_]), 2)
-    # returning:   
+    vars_ = [
+      'recency_value', 'frequency_value', 'revenue_value', 
+      'turnover_value', 'ticket_medio', 'age', 'ftd_value'
+    ]
+    # 1st model: classifier 
+    # predicting if LTV = 0
+    y_pred_clf = clf.predict(rfm_df[vars_])
+    # if y_pred_clf == 1, then is null; else is not-null
+    rfm_df["y_pred_clf"] = np.where(y_pred_clf == 1, 0, 1)
+    rfm_df
+    
+    # se some prediction is not-null
+    if any(rfm_df["y_pred_clf"] == 1):
+        # predicting lower bound for those not-null (y_pred_clf = 1):
+        rfm_df.loc[rfm_df["y_pred_clf"] == 1, "y_lower"] = predict_interval(
+            rgr,
+            rfm_df.query("y_pred_clf == 1")[vars_]
+        )["lower"]
+        rfm_df["y_lower"] = rfm_df["y_lower"].fillna(0)
+        # predicting the average for those not-null (y_pred_clf = 1):
+        rfm_df.loc[rfm_df["y_pred_clf"] == 1, "y_pred_rgr"] = predict_interval(
+          rgr, 
+          rfm_df.query("y_pred_clf == 1")[vars_]
+        )["avg"]
+        # predicting the upper bound for those not-null (y_pred_clf = 1):
+        rfm_df.loc[rfm_df["y_pred_clf"] == 1, "y_upper"] = predict_interval(
+          rgr, 
+          rfm_df.query("y_pred_clf == 1")[vars_]
+        )["upper"]
+        rfm_df["y_upper"] = rfm_df["y_upper"].fillna(0)
+        # consolidating predictions
+        rfm_df["y_pred"] = rfm_df["y_pred_clf"].astype("float")
+        # for those not null (y_pred_clf = 0)
+        rfm_df.loc[rfm_df["y_pred_clf"] == 1, "y_pred"] = rfm_df.loc[rfm_df["y_pred_clf"] == 1, "y_pred_rgr"]
+    else:
+        # consolidating predictions
+        rfm_df["y_pred"] = rfm_df["y_pred_clf"].astype("float")
+        rfm_df["y_lower"] = 0.0
+        rfm_df["y_upper"] = 0.0        
+    # rounding:
+    rfm_df['y_lower'] = np.round(rfm_df["y_lower"], 2)
+    rfm_df['y_upper'] = np.round(rfm_df["y_upper"], 2)
+    rfm_df['y_pred'] = np.round(rfm_df["y_pred"], 2)
+    
+    # returning:
     return rfm_df[["Username", "y_pred", "y_lower", "y_upper"]].to_json(orient="records")
 
 
@@ -66,8 +136,9 @@ if __name__ == '__main__':
         port = int(sys.argv[1])
     except:
         port = 1234    
-    # loading model:
-    models = load("cltv_model.joblib")
+    # loading models:
+    clf = load("clf_cltv_model.joblib")
+    rgr = load("rgr_cltv_model.joblib")
     # running debug mode:
     app.run(port=port, debug=True)
 
--- utils.py
+++ utils.py
@@ -32,14 +32,19 @@ def clean_DF(df):
 
 # function to guarantee at least 180 days:
 def f(x, min_days=181):
+    # convert to datetime
+    x["date"] = pd.to_datetime(x["date"])
+    x["registration_dt"] = pd.to_datetime(x["registration_dt"])
+    # merge
     x = pd.merge(
-        pd.DataFrame({"t1": range(min_days)}),
+        pd.DataFrame({"date": pd.date_range(x["registration_dt"].min(), periods=min_days)}),
         x,
-        on="t1",
+        on="date",
         how="left"
     )
+    # processing:
     if x.dropna().shape[0] == 0:
-        return x.dropna()
+        x= x.dropna()
     else:    
        x["ftd_value"].fillna(x["ftd_value"].dropna().unique()[0], inplace=True)    
        x["age"].fillna(x["age"].dropna().unique()[0], inplace=True)
@@ -48,13 +53,52 @@ def f(x, min_days=181):
        x["n_bets"].fillna(0, inplace=True)
        x["turnover"].fillna(0, inplace=True)
        x["ggr"].fillna(0, inplace=True)
+       x["registration_dt"].fillna(x["registration_dt"].dropna().unique()[0], inplace=True)
        return x
 
 # função para calcular Recency
-def calculate_recency(df, window=7):
-   most_recent_value = df.query("n_bets > 0")["t1"].max()
-   recency_value = window-1-most_recent_value
-   return recency_value
+def calculate_recency(df, date_max):
+    recency_value = date_max - df.query("n_bets > 0")["date"].max()
+    # Se naT (nao houve nenhuma aposta):
+    if pd.isnull(recency_value):
+        recency_value = date_max - df["registration_dt"].max()
+    recency_value = recency_value.days
+    return recency_value
+
+# helper function to get the quantiles from the trees in the Forest:
+def get_quantiles(x, q=[.05, .95]):
+   import numpy as np    
+   # calculando o q1 e q3
+   q1 = np.quantile(x, [.25])[0]
+   q3 = np.quantile(x, [.75])[0]
+   # calculando iqr:
+   iqr = q3 - q1
+   # retirando os outliers:
+   (x >= (q1 - 1.5*iqr)) & (x <= (q3 + 1.5*iqr))   
+   new_x = x[(x >= (q1 - 1.5*iqr)) & (x <= (q3 + 1.5*iqr))]   
+   # pegando o lower bound
+   lower_ = np.quantile(new_x, [q[0]])[0]
+   # calculando a média:
+   avg_ = np.mean(new_x)
+   # pegando o upper bound:
+   upper_ = np.quantile(new_x, [q[1]])[0]
+   lower_, avg_, upper_
+   return lower_, avg_, upper_
+
+# helper function to make a prediction interval:
+def predict_interval(rgr, data, q=[.1, .9]):
+    # applying preprocessing:
+    newdata = rgr["imputer"].transform(data)
+    newdata = rgr["scaling"].transform(data)
+    # predicting using each tree in the Forest:
+    all_pred = [tree.predict(newdata) for tree in rgr["regressor"].estimators_]
+    all_pred = pd.DataFrame(all_pred, columns=data.index)
+    all_pred = all_pred.T
+    # get the lower, avg and upper:
+    pred = all_pred.apply(get_quantiles, axis=1)
+    pred = pd.DataFrame(pred.to_list(), columns=["lower", "avg", "upper"], index=data.index)
+    return pred
+
 
 
 
